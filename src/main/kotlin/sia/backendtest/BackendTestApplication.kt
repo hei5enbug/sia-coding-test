09083dda312553c30f889d7565a7b9b1d2c45586
hei5enbug <kbm0598@gmail.com>
2022-10-17 16:14:11 +0900

    Initial commit

---
+++ src/main/kotlin/sia/backendtest/BackendTestApplication.kt
@@ -0,0 +1,11 @@
+package sia.backendtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendTestApplication>(*args)
+}
---
+++ src/test/kotlin/sia/backendtest/BackendTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package sia.backendtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
